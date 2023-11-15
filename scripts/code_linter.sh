ff4265c04be0ab55a57d8223ddec54e19a6bc97e
thosonit <thoson.it@gmail.com>
2023-11-15 17:51:41 +0700

    update linter

--- scripts/code_linter.sh
+++ scripts/code_linter.sh
@@ -1,6 +1,2 @@
-flutter clean
-flutter pub get
-flutter pub run intl_utils:generate
-flutter pub run build_runner build --delete-conflicting-outputs
 dart format ./lib --set-exit-if-changed
 flutter analyze
